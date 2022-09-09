de170f90f4dcf3f0f0c4120dda38e9cbc0753e34
Eduardo Tolaba <eduardoetolaba@gmail.com>
2022-09-09 10:42:55 -0300

    agregado de componentes al sitio web

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ const Navbar =()=>{
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand logo-anchor" href="a">Phone Store</a>
-                <a className="navbar-brand " href="a"><img className="bag" src="https://img.icons8.com/ios/40/000000/shopping-bag--v1.png"/></a>
+                <a className="navbar-brand " href="a"><img className="bag" src="https://img.icons8.com/ios/40/000000/shopping-bag--v1.png"/> 4</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
