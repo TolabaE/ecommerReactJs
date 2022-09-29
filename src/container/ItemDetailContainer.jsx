ef7d15fb0e26516ed01a00276917c4408f0defb2
Eduardo Tolaba <eduardoetolaba@gmail.com>
2022-09-28 23:14:57 -0300

    agregado de loading,al sitio web

--- src/container/ItemDetailContainer.jsx
+++ src/container/ItemDetailContainer.jsx
@@ -4,11 +4,12 @@ import CardDetail from '../components/CardDetail';
 import simulandoPromesa from '../utils/Promesa';
 import baseDatos from '../base/baseDatos';
 import { useParams } from 'react-router-dom';
-
+import { Ring } from '@uiball/loaders';
 
 const ItemDetailContainer = () => {
 
     const [datos, setdatos] = useState([]);
+    const [loading, setLoading] = useState(true);
     //le doy un numero a la  variable ID.
     const {id} = useParams();
     //monta los componentes,
@@ -17,7 +18,10 @@ const ItemDetailContainer = () => {
         if (id) {
             //llamo a la promesa y le agrego el array de productos traidos de una base de datos.
             simulandoPromesa(baseDatos.filter(item=>item.id == id)) //usando el metodo filter(), me filtra los datos del array y me trae el elemento que cumpla con la condicion.
-            .then (resolve=>setdatos(resolve))
+            .then (resolve=>{
+                setdatos(resolve)
+                setLoading(false)
+            })
             .catch (error=>console.log(error))
         }
     }, [id]);
@@ -25,7 +29,13 @@ const ItemDetailContainer = () => {
     return (
         <div>
             {
-                datos.map(elemen => (
+                loading
+                ?
+                <div className='ring'>
+                    <Ring size={70}/>
+                </div>
+                
+                : datos.map(elemen => (
                     <CardDetail
                     key={elemen.id}
                     imagen={elemen.image}
--- src/container/products.jsx
+++ src/container/products.jsx
@@ -4,20 +4,25 @@ import Card from '../components/Card';
 import simulandoPromesa from '../utils/Promesa';
 import baseDatos from '../base/baseDatos';
 import { NavLink, useParams } from 'react-router-dom';
+import { Ring } from '@uiball/loaders';
 
 const Products = () => {
 
     const [datos, setdatos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const { id } = useParams();//es un parametro que viene con react-router-dom.
-    //monta los componentes,
+
     useEffect(() => {
         //si no me envian un ID con el numero de categoria,traigo todos los productos de la base de datos.
         //sino me trae los productos que cumplan con esa categoria.
         if (id) {
             //llamo a la promesa y le agrego el array de productos traidos de una base de datos,usando el metodo .find()
             simulandoPromesa(baseDatos.filter(prod=>prod.category==id))
-            .then (resolve=>setdatos(resolve))
+            .then (resolve=>{
+                setdatos(resolve)
+                setLoading(false)
+            })
             .catch (error=>console.log(error))
         }else{
             simulandoPromesa(baseDatos)
@@ -26,22 +31,24 @@ const Products = () => {
         }
     }, [id]);
     return (
-        <>
-            <div className='conteiner-products'>
-                {
-                datos.map((elemento) => (
-                <Card
-                    key={elemento.id}
-                    imagen={elemento.image}
-                    marca={elemento.name}
-                    modelo={elemento.precie}
-                    detalle={<NavLink className="card-link" to={`/detalle/${elemento.id}`}>Comprar</NavLink>}
-                    //el navlink recibe como paramtro el id del producto y al hacer click,lo lleva al detalle del producto que alla seleccionado.
-                />
-                ))
-            }
+        <div className='conteiner-products'>
+            {
+            loading? 
+            <div className='ring-product'>
+                <Ring size={80}/>
             </div>
-        </>
+            : datos.map((elemento) => (
+            <Card
+                key={elemento.id}
+                imagen={elemento.image}
+                marca={elemento.name}
+                modelo={elemento.precie}
+                detalle={<NavLink className="card-link" to={`/detalle/${elemento.id}`}>Comprar</NavLink>}
+                //el navlink recibe como paramtro el id del producto y al hacer click,lo lleva al detalle del producto que alla seleccionado.
+            />
+            ))
+        }
+        </div>
     );
 }
 
